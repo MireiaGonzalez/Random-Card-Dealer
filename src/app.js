1153a9ee34490ce68ddc677fbd423864da359e89
MireiaGonzalez <mireiagonzalezz03@gmail.com>
2022-01-08 13:26:06 +0000

    exersice

--- src/app.js
+++ src/app.js
@@ -2,10 +2,53 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let cardValues = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let suits = ["spade", "club", "heart", "diamond"];
+const card = document.querySelector(".card");
+
+const randomIntFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const randomCardSuit = () => {
+  let cardSuits = ["♠", "♣", "♥", "♦"];
+  let topIcon = document.querySelector("#icon-top");
+  let bottomIcon = document.querySelector("#icon-bottom");
+  if (card.classList.contains("spade")) {
+    topIcon.innerHTML = cardSuits[0];
+    bottomIcon.innerHTML = cardSuits[0];
+  } else if (card.classList.contains("club")) {
+    topIcon.innerHTML = cardSuits[1];
+    bottomIcon.innerHTML = cardSuits[1];
+  } else if (card.classList.contains("heart")) {
+    topIcon.innerHTML = cardSuits[2];
+    bottomIcon.innerHTML = cardSuits[2];
+  } else if (card.classList.contains("diamond")) {
+    topIcon.innerHTML = cardSuits[3];
+    bottomIcon.innerHTML = cardSuits[3];
+  }
+};
+
+const randomSuit = () => {
+  let randomNum = randomIntFromInterval(1, 4);
+  console.log(randomNum - 1);
+  card.classList.add(suits[randomNum - 1]);
+  randomCardSuit();
+};
+
+window.onload = randomSuit();
